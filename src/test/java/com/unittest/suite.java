1481bfd658bab7a6a6d40313844ee69a2f3f3bbf
yetun.thu <L00187723@atu.ie>
2024-11-23 00:40:11 +0000

    (Dev):The Korok Seed Guide Creation to close the issue #3
    close #3

--- src/test/java/com/unittest/suite.java
+++ src/test/java/com/unittest/suite.java
@@ -4,7 +4,7 @@ import org.junit.platform.suite.api.SelectClasses;
 import org.junit.platform.suite.api.Suite;
 
 @Suite
-@SelectClasses({AppTest.class, KorokSeedGuideTest.class, GameBuildingsTest.class})
+@SelectClasses({KorokSeedGuideTest.class, GameBuildingsTest.class})
 
 public class suite {
     
